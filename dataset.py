084fc201e9872fbcc592edff482df64ca5f7c73e
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2023-06-13 21:45:55 +0200

    (feat) adds EfficientNet3 model for training
    Signed-off-by: Alexander Gemmingen <alexander.gemmingen@gmx.de>

---
+++ dataset.py
@@ -0,0 +1,46 @@
+import csv
+import os
+import shutil
+import collections
+
+class Dataset:
+    labelList = []
+    path = "rawData/images/"
+
+    def __init__(self):
+        self.setupDataset()
+
+    def setupDataset(self):
+        with open(self.path + "_datasetInfo.csv") as csvfile:
+            reader = csv.reader(csvfile, delimiter=",")
+            # for row, get image
+            # Rename image to indexnr.jpg
+            # get label and save as lists
+            index = 0
+            for row in reader:
+                index += 1
+                id = row[0]
+                label = row[1]
+                # trust = row[2]
+                # src = row[3] maybe needed in the future
+                # Use below 2 lines to regression model
+                # label = self.normalizeLabel(int(label))
+                # index = self.createData(id, index, label, trust)
+                self.createCategories(label, id)
+
+    def getLabelList(self):
+        print(self.labelList)
+        print(len(self.labelList))
+        return self.labelList
+
+    def normalizeLabel(self, label):
+        return round(int(label) / 100)
+    
+    def createCategories(self, label, id):
+        rootDir = "data/"
+        normalizedLabel = str(self.normalizeLabel(label))
+        imagePath = self.path + id + ".jpg"
+        path = rootDir + "/" + normalizedLabel
+        if not os.path.exists(path):
+            os.makedirs(path)
+        shutil.copyfile(imagePath, path + "/" + id + ".jpg")
---
+++ utils.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+def plot_accuracy(h, accType):
+    plt.plot(h.history[accType])
+    plt.plot(h.history["val_" + accType])
+    plt.title("model accuracy")
+    plt.ylabel("MAE")
+    plt.xlabel("epoch")
+    plt.legend(["train", "validation"], loc="upper left")
+    plt.show()
